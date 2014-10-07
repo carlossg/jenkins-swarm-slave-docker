a9f420a882031e65c77a0da2f2c90494afc4e90b
Carlos Sanchez <csanchez@maestrodev.com>
2014-10-07 21:26:10 +0200

    Allow easily running a shell in the image

--- jenkins-slave.sh
+++ jenkins-slave.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # jenkins swarm slave
 JAR=`ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1`
@@ -8,5 +8,11 @@ if [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
   PARAMS="-master http://$JENKINS_PORT_8080_TCP_ADDR:$JENKINS_PORT_8080_TCP_PORT"
 fi
 
-echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
-exec java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
+# if `docker run` first argument start with `-` the user is passing jenkins swarm launcher arguments
+if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
+  echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
+  exec java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
+fi
+
+# As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
+exec $@
