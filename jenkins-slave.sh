33142932d09c42c0b8b6462605ec74df2fe9c45b
Carlos Sanchez <csanchez@maestrodev.com>
2014-10-09 14:39:54 +0200

    Use JENKINS_PORT_8080_TCP_ADDR only if -master is not provided

--- jenkins-slave.sh
+++ jenkins-slave.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# jenkins swarm slave
-JAR=`ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1`
-
-# if using --link jenkins:jenkins
-if [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
-  PARAMS="-master http://$JENKINS_PORT_8080_TCP_ADDR:$JENKINS_PORT_8080_TCP_PORT"
-fi
-
 # if `docker run` first argument start with `-` the user is passing jenkins swarm launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
+
+  # jenkins swarm slave
+  JAR=`ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1`
+
+  # if -master is not provided and using --link jenkins:jenkins
+  if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
+    PARAMS="-master http://$JENKINS_PORT_8080_TCP_ADDR:$JENKINS_PORT_8080_TCP_PORT"
+  fi
+
   echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
   exec java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
 fi
