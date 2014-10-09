178b93e42405ad81bb6940398870f250b08a366b
Carlos Sanchez <csanchez@maestrodev.com>
2014-10-09 13:26:54 +0200

    Quote exec arguments

--- jenkins-slave.sh
+++ jenkins-slave.sh
@@ -15,4 +15,4 @@ if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
-exec $@
+exec "$@"
